6ae9982e77a671b2f5a07d6dbf42fd918af688f5
jimfox <fox@washington.edu>
2014-06-17 09:17:20 -0700

    fix null cert display

--- war/sp.js
+++ war/sp.js
@@ -955,7 +955,7 @@ function proxy_saveProxy(entityId) {
    if (gcid!='') xml += '<ProxyIdp idp="Google" clientId="' + iam_makeOkXml(gcid) + '" clientSecret="' + iam_makeOkXml(gcpw) + '"/>';
    if (fcid!='') xml += '<ProxyIdp idp="Facebook" clientId="' + iam_makeOkXml(fcid) + '" clientSecret="' + iam_makeOkXml(fcpw) + '"/>';
    xml += '</Proxy></Proxys>';
-   console.log(xml);
+   // console.log(xml);
    var url = v_root + v_vers + '/rp/proxy?id=' + entityId + '&xsrf=' + v_xsrf + adminQS;
    iam_putRequest(url, null, xml, null, _postSaveProxy);
 }
