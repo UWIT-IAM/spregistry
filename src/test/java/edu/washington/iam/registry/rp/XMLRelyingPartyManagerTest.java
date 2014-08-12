0a9a7dde1a7acb1659fbe4a3388e88a5374e9de3
Jeff Franklin <jpf@washington.edu>
2014-08-12 16:26:39 -0700

    Add a simple unit test

---
+++ src/test/java/edu/washington/iam/registry/rp/XMLRelyingPartyManagerTest.java
@@ -0,0 +1,26 @@
+package edu.washington.iam.registry.rp;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by jpf on 8/12/14.
+ */
+public class XMLRelyingPartyManagerTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGenRelyingPartyByName() throws Exception {
+        XMLRelyingPartyManager rpm = new XMLRelyingPartyManager();
+        RelyingParty rp = rpm.genRelyingPartyByName("https://www.example.com", "www.example.com");
+        Assert.assertEquals("https://www.example.com", rp.getEntityId());
+    }
+}
