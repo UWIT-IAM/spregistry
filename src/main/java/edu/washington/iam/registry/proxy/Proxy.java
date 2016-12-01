6bde31073d967a854651d5b03e3d65e53dca3914
Matt MacAdam <mattjm@uw.edu>
2016-11-30 16:27:27 -0800

    gateway tab work (Release candidate) plu history tab work so far.  Up to 2017-12-11

--- src/main/java/edu/washington/iam/registry/proxy/Proxy.java
+++ src/main/java/edu/washington/iam/registry/proxy/Proxy.java
@@ -43,14 +43,15 @@ public class Proxy implements Serializable  {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
+    private String uuid;
     private String entityId;
     private boolean socialActive;
+
 
     private String safePy(String in) {
        return in.replaceAll("\"","\\\"");
     }
 
-    // create from document element
     public Proxy (){}
 
     //2017-11-13 mattjm constructor taking XML document as argument removed (and deleted XMLProxyManager)
@@ -68,6 +69,9 @@ public class Proxy implements Serializable  {
     }
     public boolean getSocialActive() { return (socialActive); }
 
-
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+    public String getUuid() { return uuid; }
 }
 
