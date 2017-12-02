37356745fb20911caa542af6f96538486dbbf608
mattjm <mattjm@uw.edu>
2017-12-01 16:44:50 -0800

    fixing proxy tab text--figuring out how to get the list to display properly.

--- src/main/java/edu/washington/iam/registry/ws/RelyingPartyController.java
+++ src/main/java/edu/washington/iam/registry/ws/RelyingPartyController.java
@@ -453,6 +453,7 @@ public class RelyingPartyController {
 
     private ModelAndView loginPage(HttpServletRequest request, HttpServletResponse response, int method) {
         String remoteUser = request.getRemoteUser();
+        remoteUser = "mattjm@washington.edu";
         log.debug("social login attempt, shib remoteUser value: " + remoteUser);
         if (method==0) {  // social login
            String idp = (String)request.getAttribute("Shib-Identity-Provider");
